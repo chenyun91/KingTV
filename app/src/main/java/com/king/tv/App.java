3ba262551fc47b13457680ed99b607060359b653
chenyun <chenyun@wzdai.com>
2018-07-09 10:39:43 +0800

    去掉bugly，解决项目包重复问题

--- app/src/main/java/com/king/tv/App.java
+++ app/src/main/java/com/king/tv/App.java
@@ -4,16 +4,11 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
-import com.king.base.util.LogUtils;
-import com.king.thread.nevercrash.NeverCrash;
 import com.king.tv.dao.greendao.DaoMaster;
 import com.king.tv.dao.greendao.DaoSession;
 import com.king.tv.di.component.AppComponent;
 import com.king.tv.di.component.DaggerAppComponent;
 import com.king.tv.di.module.AppModule;
-import com.tencent.bugly.Bugly;
-import com.tencent.bugly.beta.Beta;
-import com.tencent.bugly.crashreport.CrashReport;
 
 /**
  * @author Jenly <a href="mailto:jenly1314@gmail.com">Jenly</a>
@@ -35,8 +30,8 @@ public class App extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
 
-        MultiDex.install(base);
-        Beta.installTinker();
+        MultiDex.install(this);
+//        Beta.installTinker();
     }
 
     @Override
@@ -44,15 +39,15 @@ public class App extends Application {
         super.onCreate();
         initDatabase();
         // 调试时，将第三个参数改为true
-        Bugly.init(this,BUGLY_ID,false);
+//        Bugly.init(this,BUGLY_ID,false);
         mAppComponent = DaggerAppComponent.builder().appModule(new AppModule(this,Constants.BASE_URL)).build();
 
-        NeverCrash.init(new NeverCrash.CrashHandler() {
-            @Override
-            public void uncaughtException(Thread t, Throwable e) {
-                CrashReport.postCatchedException(e);
-            }
-        });
+//        NeverCrash.init(new NeverCrash.CrashHandler() {
+//            @Override
+//            public void uncaughtException(Thread t, Throwable e) {
+//                CrashReport.postCatchedException(e);
+//            }
+//        });
     }
 
 
